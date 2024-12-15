74b6c44af5832f0bbee326af96ae5d9e152b37a7
Jaswanth <86583861+jaswanth-damarla@users.noreply.github.com>
2024-12-15 12:16:29 +0530

    areaofSquare.java

---
+++ areaofSquare.java
@@ -0,0 +1,13 @@
+2. import java.util.*;
+
+      public class HW{
+          public static void main(String arg[]){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the side of a square: ");
+            int side = sc.nextInt();
+
+            int area = side * side;
+
+           System.out.println(area);
+      }
+   }
