6151b169a2b66d841e8e767b40a846039a81b9cb
Jaswanth <86583861+jaswanth-damarla@users.noreply.github.com>
2024-12-16 03:00:12 +0000

    To print pattern of stars

---
+++ F9.java
@@ -0,0 +1,14 @@
+public class F9{
+    public static void main(String arg[]){
+
+       int i;
+       int j;
+       int n=5;
+       for(i=1; i<=n; i++){
+        for(j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+    }
+}
