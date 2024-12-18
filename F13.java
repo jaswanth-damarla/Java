bbd19a06b2b3b812bd2ecfcc7e1055e1547cf731
Jaswanth <86583861+jaswanth-damarla@users.noreply.github.com>
2024-12-18 10:15:23 +0000

    To check a number is odd or even using functions

--- F13.java
+++ F13.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 public class F13{
     public static void main(String arg[]){
-        int a = 2;
-        int b = 4;
-        int c = 6;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int a = sc.nextInt();
 
-        int av = avg(a,b,c);
-
-        System.out.println("Average of three numbers is : " + av);
+        System.out.println(isEven(a));
 
     }
 
-    public static int avg(int a, int b, int c){
-        int sum = a + b + c;
-        int avg = sum/3;
-
-        return avg;
+    public static boolean isEven(int a){
+        if (a % 2 == 0) {
+            return true; // Return true if the number is even
+        } else {
+            return false; // Return false if the number is odd
+        }
     }
 }
