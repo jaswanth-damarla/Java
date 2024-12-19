1ae4fbf782325aa0c20a08e2113f25eee29b1790
Jaswanth <86583861+jaswanth-damarla@users.noreply.github.com>
2024-12-19 14:08:53 +0000

    To check if a number is palindrome or not

---
+++ F15.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class F15{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int re = revnum(num);
+        if(re == 1){
+            System.out.println("The number " + num + " is a palindrome");
+        }else{
+            System.out.println("The number " + num + " is not a palindrome");
+        }
+
+    }
+
+    public static int revnum(int num){
+        
+        int Onum = num;//Original number
+        int lastd = num % 10;
+        int revn = 0;
+
+        while(num > 0){
+        revn = revn * 10 + lastd;
+        num = num / 10;
+        lastd = num % 10;
+        }
+
+        if(revn == Onum){
+            return 1;
+        }else{
+            return 0; 
+        }
+    }
+}
