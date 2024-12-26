19653748888c0339ff88ff2dad482d124c8da1f2
Jaswanth <86583861+jaswanth-damarla@users.noreply.github.com>
2024-12-26 15:46:47 +0000

    Linear Search in an array

---
+++ F25.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class F25{
+    public static void main(String args[]){
+        int n[] = {1,2,3,34,5,6,7,8,9,10};
+        int key = 2;
+        int index = LinearSearch(n,key);
+        if(index == -1){
+            System.out.println("Element was not found");
+        }else{
+            System.out.println("Element was found at index: " + index);
+        }
+    }
+    public static int LinearSearch(int n[], int key){
+
+        int i;
+        for(i=0; i<n.length; i++){
+            if(n[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    
+    }
+
+}
